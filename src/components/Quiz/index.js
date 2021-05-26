38c5b58f9b351d05a41e8be6d558700bf3e4f137
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-05-27 00:41:43 +0800

    Used the entropiya quiz api.

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,58 +1,38 @@
-import {useEffect, useState} from 'react';
-import {econ} from '../../data/econ';
-import {MathJax, MathJaxContext} from 'better-react-mathjax';
-// import parse from 'html-react-parser'
+import React, {useEffect, useState} from 'react';
+import API from '../../api';
+import {Text} from 'react-native';
 
-const config = {
-  loader: {load: ['[tex]/html']},
-  tex: {
-    packages: {'[+]': ['html']},
-    inlineMath: [
-      ['$', '$'],
-      ['\\(', '\\)'],
-    ],
-    displayMath: [
-      ['$$', '$$'],
-      ['\\[', '\\]'],
-    ],
-  },
-};
-
-const Quiz = () => {
-  const [categories, setCategories] = useState([]);
-  const fetchQuizCategories = () => {
-    const formattedData = econ.map(category => {
-      const incorrectAnswersIndexes = category.incorrect_answers.length;
+const QuizCategories = () => {
+  const [questions, setQuestions] = useState([]);
+  const fetchQuizCategories = async () => {
+    const config = {
+      header: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const {data} = await API.get('quiz/questions', config);
+    const formattedData = data.map(question => {
+      const incorrectAnswersIndexes = question.incorrect_answers.length;
       const randomIndex = Math.random() * (incorrectAnswersIndexes - 0) + 0;
-      category.incorrect_answers.splice(
+      question.incorrect_answers.splice(
         randomIndex,
         0,
-        category.correct_answer,
+        question.correct_answer,
       );
       return {
-        ...category,
-        answers: category.incorrect_answers,
+        ...question,
+        answers: question.incorrect_answers,
       };
     });
-    setCategories(formattedData);
+    setQuestions(formattedData);
   };
 
   useEffect(() => {
     fetchQuizCategories();
   }, []);
 
-  console.log({categories});
-
-  // return <div>{parse(results[0].question)}</div>
-  return (
-    <MathJaxContext version={3} config={config}>
-      <MathJax>
-        <div dangerouslySetInnerHTML={{__html: econ[0].question}}></div>
-      </MathJax>
-    </MathJaxContext>
-  );
+  console.log(questions);
+  return <Text>Hello from Quiz Categories!</Text>;
 };
 
-export default Quiz;
-
-// hideUntilTypeset={'first'} attribute ng MathJax
+export default QuizCategories;
